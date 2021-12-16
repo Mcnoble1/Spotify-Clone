f6f0a8c5bed83aa02c9c3a2bcbf8ed389bf862d0
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2021-12-16 10:18:58 +0100

    Sertup build process and dependencies :octocat:

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import "reset-css";
+
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
+
